479e0b0869d0145368848e053a93b19dbc90406d
Dr. Kerem Koseoglu <kerem@keremkoseoglu.com>
2022-04-14 20:51:14 +0300

    fixes #3

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 """ Sahibinden main module """
 from sahibinden.search import Search
 
-search = Search("https://www.sahibinden.com/arazi-suv-pickup-nissan-qashqai/benzin/manuel?a277_max=2013&a277_min=2013&a276_min=100000&a276_max=200000")
-for price in search.result.prices:
-    print(str(price))
-print(search.result.price_median)
+def run_test():
+    """ Just a test """
+    search = Search("https://www.sahibinden.com/arazi-suv-pickup-nissan-qashqai/benzin/manuel?a277_max=2013&a277_min=2013&a276_min=100000&a276_max=200000")
+    for price in search.result.prices:
+        print(str(price))
+    print(search.result.price_median)
+
+if __name__ == "__main__":
+    run_test()
