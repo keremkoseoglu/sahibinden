bda549d9ea85e1ae45e2b25ca49acdd32a8d6db3
Dr. Kerem Koseoglu <kerem@keremkoseoglu.com>
2022-10-01 18:52:51 +0300

    stuff

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import test
 
 if __name__ == "__main__":
-    # test.regular_search()
+    test.regular_search()
     # test.quick_median_single_page()
     # test.quick_median_multi_page_no_offset()
     # test.quick_median_multi_page_with_offset()
